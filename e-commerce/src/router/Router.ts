645992341a3418b85475169c860263583098d5a2
anturchin <tur4intwo@gmail.com>
2024-05-12 18:53:38 +0300

    feat: add router

---
+++ e-commerce/src/router/Router.ts
@@ -0,0 +1,31 @@
+import { IRouter } from './Router.interface';
+import { IRoute, RoutePath } from './types';
+
+export class Router implements IRouter {
+    private routes: IRoute[] = [];
+
+    constructor(routes: IRoute[]) {
+        this.addRoute(routes);
+    }
+
+    public addRoute(routes: IRoute[]): void {
+        routes.forEach((route) => this.routes.push(route));
+    }
+
+    public findRoute(path: RoutePath): IRoute | undefined {
+        return this.routes.find((r) => r.path === path);
+    }
+
+    public showNotFoundPage(): void {
+        this.findRoute(RoutePath.NOT_FOUND)?.callback();
+    }
+
+    public async navigate(path: RoutePath): Promise<void> {
+        const route = this.findRoute(path);
+        if (route) {
+            await route.callback();
+        } else {
+            this.showNotFoundPage();
+        }
+    }
+}
