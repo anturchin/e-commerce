b4fd185189f63ec13fa452f7bebefed8b73a750a
Dasha <girl.is.anime20@gmail.com>
2024-05-17 13:25:24 +0300

    add form registration

--- e-commerce/src/views/pages/registration/Registration.ts
+++ e-commerce/src/views/pages/registration/Registration.ts
@@ -1,14 +1,19 @@
 import { View } from '../../View';
+import './Registration.scss';
+import { Form } from './form/Form';
 
 export class Registration extends View {
     constructor() {
-        super({ tag: 'section' });
+        super({ tag: 'section', classNames: ['registration-page'] });
         this.setupRegistration();
     }
 
     private setupRegistration(): void {
         const h1 = document.createElement('h1');
-        h1.textContent = 'Registration';
+        h1.textContent = 'Create your account';
         this.viewHtmlElement.addInnerElement(h1);
+
+        const formRegistration = new Form().getElement();
+        this.viewHtmlElement.addInnerElement(formRegistration);
     }
 }
