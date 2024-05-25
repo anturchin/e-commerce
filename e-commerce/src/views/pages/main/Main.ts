50a1e035c41f86f1a3a509dee20c705cc39db35e
Dasha <girl.is.anime20@gmail.com>
2024-05-25 17:16:34 +0300

    fix: fixed static card

--- e-commerce/src/views/pages/main/Main.ts
+++ e-commerce/src/views/pages/main/Main.ts
@@ -8,7 +8,13 @@ export class Main extends View {
     }
 
     private setupMain(): void {
-        const cardProduct = new Card().getElement();
+        const cardProduct = new Card(
+            'https://the-istore.ru/upload/iblock/f00/f0002b9d554706e0cafffc7f318d6fbf/4a5cbbe090879a8089739552a81d0bb1.jpeg',
+            'Iphone 15',
+            'Iphone 15 with 128 GB storage',
+            '$1000',
+            '$999'
+        ).getElement();
         this.viewHtmlElement.addInnerElement(cardProduct);
     }
 }
