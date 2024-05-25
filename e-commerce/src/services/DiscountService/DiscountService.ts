46bee4c1cb14824611a7d0d035fc272281b0c85e
farsenyev <farsenyev@bk.ru>
2024-05-25 20:40:43 +0300

    feat: add service fot getting discounts

---
+++ e-commerce/src/services/DiscountService/DiscountService.ts
@@ -0,0 +1,37 @@
+import { ICustomerResponseFailed } from '../types';
+import { IDiscountResponse } from './types';
+
+export class DiscountService {
+    private static readonly projectKey: string = 'fad-team';
+
+    private static readonly API_URL: string = 'https://api.europe-west1.gcp.commercetools.com';
+
+    static async getDiscounts(
+        BEARER_TOKEN: string
+    ): Promise<IDiscountResponse | ICustomerResponseFailed> {
+        const URL = `${this.API_URL}/${this.projectKey}/product-discounts`;
+
+        try {
+            const resp = await fetch(URL, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${BEARER_TOKEN}`,
+                    'Content-type': 'application/json',
+                },
+            });
+
+            if (!resp.ok) {
+                const data = await resp.json();
+                return {
+                    statusCode: data.statusCode,
+                    msg: data.message,
+                } as ICustomerResponseFailed;
+            }
+
+            return (await resp.json()) as IDiscountResponse;
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+}
