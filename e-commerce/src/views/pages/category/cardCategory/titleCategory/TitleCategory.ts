8b1e29d0cb735a33c011b54220ea565dac1d5d94
Dasha <girl.is.anime20@gmail.com>
2024-05-28 11:39:27 +0300

    refactor: change code with ul li structure

--- e-commerce/src/views/pages/category/cardCategory/titleCategory/TitleCategory.ts
+++
@@ -1,8 +0,0 @@
-import { View } from '../../../../View';
-import './TitleCategory.scss';
-
-export class TitleCategory extends View {
-    constructor(categoryTitle: string) {
-        super({ tag: 'p', classNames: ['title__category'], textContent: categoryTitle });
-    }
-}
