1e536d4dfa92a228dc982718c8ae2e09c0cb9e26
Dasha <girl.is.anime20@gmail.com>
2024-05-29 22:00:14 +0300

    feat: add price/sale

--- e-commerce/src/views/fullInfoProduct/FullInfoProduct.ts
+++ e-commerce/src/views/fullInfoProduct/FullInfoProduct.ts
@@ -4,26 +4,44 @@ import { NameProduct } from './nameProduct/NameProduct';
 import { ImgProduct } from './imgProduct/ImgProduct';
 import { ContainerImg } from './containerForImg/ContainerForImg';
 import { ContainerColumn } from './container/Container';
+import { PriceProduct } from './priceProduct/PriceProduct';
+import { SaleProduct } from './saleProduct/SaleProduct';
 
 import './FullInfoProduct.scss';
 
 export class FullInfoProduct extends View {
-    constructor(productTitle: string, productImg: string, productDescription: string) {
+    constructor(
+        productTitle: string,
+        productImg: string,
+        productDescription: string,
+        productPrice: string,
+        productSale: string
+    ) {
         super({ tag: 'div', classNames: ['product__info'] });
-        this.setupProductInfo(productTitle, productImg, productDescription);
+        this.setupProductInfo(
+            productTitle,
+            productImg,
+            productDescription,
+            productPrice,
+            productSale
+        );
     }
 
     private setupProductInfo(
         productTitle: string,
         productImg: string,
-        productDescription: string
+        productDescription: string,
+        productPrice: string,
+        productSale: string
     ): void {
         const containerInfo = new ContainerColumn().getElement();
 
         const nameProduct = new NameProduct(productTitle).getElement();
         const descriptionProduct = new DescriptionProduct(productDescription).getElement();
+        const price = new PriceProduct(productPrice).getElement();
+        const sale = new SaleProduct(productSale).getElement();
 
-        containerInfo.append(nameProduct, descriptionProduct);
+        containerInfo.append(nameProduct, descriptionProduct, price, sale);
         this.viewHtmlElement.addInnerElement(containerInfo);
 
         const containerImg = new ContainerImg().getElement();
