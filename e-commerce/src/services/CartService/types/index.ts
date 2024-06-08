60269e9f12c8d14a28a36a427d00d6702663b688
farsenyev <farsenyev@bk.ru>
2024-06-08 23:57:33 +0300

    fix: fix lint

--- e-commerce/src/services/CartService/types/index.ts
+++ e-commerce/src/services/CartService/types/index.ts
@@ -1,3 +1,79 @@
+interface Dimensions {
+    w: number;
+    h: number;
+}
+
+interface Image {
+    url: string;
+    dimensions: Dimensions;
+}
+
+interface Value {
+    type: string;
+    fractionDigits: number;
+    currencyCode: string;
+    centAmount: number;
+}
+
+interface Price {
+    value: Value;
+    id: string;
+}
+
+interface Variant {
+    id: number;
+    sku: string;
+    prices: Price[];
+    images: Image[];
+    attributes: any[];
+    assets: any[];
+}
+
+interface ProductType {
+    typeId: string;
+    id: string;
+    version: number;
+}
+
+interface StateType {
+    typeId: string;
+    id: string;
+}
+
+interface State {
+    quantity: number;
+    state: StateType;
+}
+
+interface CustomType {
+    typeId: string;
+    id: string;
+}
+
+interface Custom {
+    type: CustomType;
+    fields: Record<string, string>;
+}
+
+interface LineItem {
+    id: string;
+    productId: string;
+    name: Record<string, string>;
+    productType: ProductType;
+    productSlug: Record<string, string>;
+    variant: Variant;
+    price: Price;
+    quantity: number;
+    discountedPricePerQuantity: any[];
+    state: State[];
+    priceMode: string;
+    lineItemMode: string;
+    totalPrice: Price;
+    custom?: Custom;
+    perMethodTaxRate: any[];
+    taxedPricePortions: any[];
+}
+
 export interface ICart {
     type: string;
     id: string;
@@ -20,82 +96,6 @@ export interface ICart {
     shipping: any[];
     shippingMode: string;
     country?: string;
-}
-
-interface LineItem {
-    id: string;
-    productId: string;
-    name: Record<string, string>;
-    productType: ProductType;
-    productSlug: Record<string, string>;
-    variant: Variant;
-    price: Price;
-    quantity: number;
-    discountedPricePerQuantity: any[];
-    state: State[];
-    priceMode: string;
-    lineItemMode: string;
-    totalPrice: Price;
-    custom?: Custom;
-    perMethodTaxRate: any[];
-    taxedPricePortions: any[];
-}
-
-interface ProductType {
-    typeId: string;
-    id: string;
-    version: number;
-}
-
-interface Variant {
-    id: number;
-    sku: string;
-    prices: Price[];
-    images: Image[];
-    attributes: any[];
-    assets: any[];
-}
-
-interface Price {
-    value: Value;
-    id: string;
-}
-
-interface Value {
-    type: string;
-    fractionDigits: number;
-    currencyCode: string;
-    centAmount: number;
-}
-
-interface Image {
-    url: string;
-    dimensions: Dimensions;
-}
-
-interface Dimensions {
-    w: number;
-    h: number;
-}
-
-interface State {
-    quantity: number;
-    state: StateType;
-}
-
-interface StateType {
-    typeId: string;
-    id: string;
-}
-
-interface Custom {
-    type: CustomType;
-    fields: Record<string, string>;
-}
-
-interface CustomType {
-    typeId: string;
-    id: string;
 }
 
 export interface ICartResponse {
