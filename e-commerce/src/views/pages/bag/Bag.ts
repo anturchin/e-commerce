fec0e606c733b3127797b657f42f7daef20c833f
Dasha <girl.is.anime20@gmail.com>
2024-06-09 15:01:50 +0300

    feat: add card view in bag

--- e-commerce/src/views/pages/bag/Bag.ts
+++ e-commerce/src/views/pages/bag/Bag.ts
@@ -1,8 +1,43 @@
 import { View } from '../../View';
+import { CardBagList } from './cardListBag/CardListBag';
 import './Bag.scss';
 
+export interface IBagCards {
+    url: string;
+    name: string;
+    price: string;
+    sale: string;
+}
+
+const cardsProduct: IBagCards[] = [
+    {
+        url: 'https://cdsassets.apple.com/live/7WUAS350/images/iphone/iphone-14-pro-max-colors.png',
+        name: 'Iphone 15',
+        price: '900$',
+        sale: '899$',
+    },
+    {
+        url: 'https://cdsassets.apple.com/live/7WUAS350/images/iphone/iphone-14-pro-max-colors.png',
+        name: 'Iphone 15',
+        price: '900$',
+        sale: '899$',
+    },
+];
+
 export class Bag extends View {
+    private productBag: CardBagList | null = null;
+
     constructor() {
         super({ tag: 'section', classNames: ['content', 'bag'] });
+        this.renderProductBagList();
+    }
+
+    public renderProductBagList(props: IBagCards[] = cardsProduct) {
+        this.productBag = new CardBagList(props);
+        this.viewHtmlElement.addInnerElement(this.productBag.getElement());
+    }
+
+    public getWrapperList(): CardBagList | null {
+        return this.productBag;
     }
 }
---
+++ e-commerce/src/views/pages/bag/cardBag/titleProduct/TitleProduct.ts
@@ -0,0 +1,8 @@
+import { View } from '../../../../View';
+import './TitleProduct.scss';
+
+export class TitleProduct extends View {
+    constructor(cardTitle: string) {
+        super({ tag: 'h4', classNames: ['card__title'], textContent: cardTitle });
+    }
+}
